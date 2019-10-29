8da3ecbb3822424c013c3569424751792917e3f1
Amy Huang <ahuang@ucvts.org>
2019-10-29 14:46:48 -0400

    first

--- temp.java
+++ temp.java
@@ -7,18 +7,27 @@ public class temp {
 
     String scale;
     String convert;
+    int temp;
     do{
         System.out.print("Enter an origin scale: ");
         scale = in.nextLine();
     
             if(scale == "F" || scale == "f" || scale == "C" || scale == "c"){
-                System.out.print("Enter a destination: ");
-                convert = in.nextLine();
+                
+                do{
+                    System.out.print("Enter a destination: ");
+                    convert = in.nextLine();
+                }while(scale == "F" || scale == "f" || scale == "C" || scale == "c");
+
+                System.out.print("Enter a temperature: ");
+                temp = in.nextInt();
             }
     }while(scale != "F" || scale != "f"|| scale != "C" || scale != "c");
     
     if(scale = "F") {
-        if(convert = "C")
+        if(convert = "C"){
+            
+        }
     }
 
   }
