d7ab45a20f399faaefaa611fd5de81285c011431
Amy Huang <ahuang@ucvts.org>
2019-10-29 14:28:26 -0400

    Add files via upload

---
+++ temp.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class temp {
+
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+
+    String scale;
+    String convert;
+    do{
+        System.out.print("Enter an origin scale: ");
+        scale = in.nextLine();
+    
+            if(scale == "F" || scale == "f" || scale == "C" || scale == "c"){
+                System.out.print("Enter a destination: ");
+                convert = in.nextLine();
+            }
+    }while(scale != "F" || scale != "f"|| scale != "C" || scale != "c");
+    
+    if(scale = "F") {
+        if(convert = "C")
+    }
+
+  }
+
+}
+
+
